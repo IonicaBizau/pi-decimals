d883daedd37eb483ddb3b5f6fcd119df62e398c1
Ionică Bizău <bizauionica@gmail.com>
2017-10-11 14:05:24 +0300

    Rename into pi-decimals

--- example/index.js
+++ example/index.js
@@ -3,8 +3,8 @@
 const pi = require("..");
 
 // Get the first 42 digits
-// Note: the `digits` array doesn't contain "3."
-console.log("3." + pi.digits.slice(0, 42).join(""))
+// Note: the `decimals` array doesn't contain "3."
+console.log("3." + pi.decimals.slice(0, 42).join(""))
 // => "3.1415926535897932384626433832795028841971"
 
 // Get the path to the txt file (could be useful to
