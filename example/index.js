b1605b89c83ac97580f835a53f730778fc3c7a37
Ionică Bizău <bizauionica@gmail.com>
2017-10-11 13:51:38 +0300

    :arrow_up: 1.0.0 :tada:

--- example/index.js
+++ example/index.js
@@ -1,5 +1,12 @@
 "use strict";
 
-const piDigits = require("../lib");
+const pi = require("..");
 
-console.log(piDigits());
+// Get the first 42 elements
+console.log(pi.digits.slice(0, 42).join(""))
+// => "3.1415926535897932384626433832795028841971"
+
+// Get the path to the txt file (could be useful to
+// other things such as streaming or whatever)
+console.log(pi.txt_path)
+// => "/Users/.../pi-digits/lib/pi.txt"
